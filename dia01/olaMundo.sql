c57d21de648e42d38fdbf514f4fdca9ab0b60dea
geniedson@hotmail.com <geniedson@hotmail.com>
2023-05-25 00:05:16 +0000

    Olá mundo do primeiro dia

---
+++ dia01/olaMundo.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+select 'Hola Mundo'
+
+-- COMMAND ----------
+
+select * --selecione todas as tabelas
+from silver.pizza_query.pedido --da tabela silver.pizza_query.pedido
+
+-- COMMAND ----------
+
+
